3c888355d3f282a0cfc81184e251a044c6efdedd
Glenn Vinbladh <glenn@edgeweb.se>
2017-12-10 21:09:34 +0100

    some auth basics

---
+++ sveco/src/Controller/SecurityController.php
@@ -0,0 +1,38 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends Controller
+{
+
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(Request $request, AuthenticationUtils $authUtils)
+    {
+        // get the login error if there is one
+        $error = $authUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ));
+    }
+
+    /**
+     * @Route("/login", name="failed_login")
+     */
+    public function failed_login(Request $request, AuthenticationUtils $authUtils)
+    {
+        echo "Failed!";
+    }
+
+}
