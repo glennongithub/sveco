b5594b3738901b66fa4aac4579c79d15be05c9b2
Glenn Vinbladh <glenn@edgeweb.se>
2017-12-12 11:23:04 +0100

    improving vagrant

--- after.sh
+++ after.sh
@@ -3,3 +3,7 @@
 # If you would like to do some extra provisioning you may
 # add any commands you wish to this file and they will
 # be run after the Homestead machine is provisioned.
+
+cd /vagrant/sveco
+# make sure everything is installed
+composer install
