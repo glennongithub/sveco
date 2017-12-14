e7161e82da3a8a93dc8a296d15b95f01d1990a81
Glenn Vinbladh <glenn@edgeweb.se>
2017-12-14 08:54:40 +0100

    webapp added again but right this time I hope

--- after.sh
+++ after.sh
@@ -17,25 +17,32 @@ php bin/console doctrine:migrations:migrate
 ###
 
 echo "--> Installing NodeJS... "
-# clean out old node_modules dirs
-sudo rm -fr /home/vagrant/webapp/node_modules
+
 
 # install node - http://tecadmin.net/install-latest-nodejs-npm-on-ubuntu/#
-sudo /bin/sh -c "curl -sL https://deb.nodesource.com/setup_6.x | bash -" > /dev/null 2>&1
-sudo apt-get -y install nodejs libjpeg-turbo8 build-essential pkg-config libpng-dev > /dev/null 2>&1
+# sudo /bin/sh -c "curl -sL https://deb.nodesource.com/setup_6.x | bash -" > /dev/null 2>&1
+# sudo apt-get -y install nodejs libjpeg-turbo8 build-essential pkg-config libpng-dev > /dev/null 2>&1
 
 # install ionic globaly
-sudo npm install -g cordova ionic > /dev/null
+# sudo npm install -g cordova ionic > /dev/null
 
-echo "--> Install NPM modules in working dir: '`pwd`'..."
-# install dependencies
-npm update > /dev/null
 
-# create new ionic app so we can symlink that to public
-# if it is not already created
+# if ionic app added to this project .. symlink it
+# symlink it to public to we can access app-interface via webserver only if it's exist
 if [ ! -f /home/vagrant/code/webapp ]; then
-    ionic start webapp tabs
+    #cd /home/vagrant/code/webapp
+    # if this deos not happen the repo need to manualy have someone create a ionic app named webapp  in root
+    # like this --> ionic start webapp tabs --no-git
+    # echo "--> Install NPM modules in working dir: '`pwd`'..."
+    # install dependencies
+    # npm update > /dev/null
+
+    # clean out old node_modules dirs .-- this will have to be updated/installed from npm via host
+    sudo rm -fr /home/vagrant/code/webapp/node_modules
+
+    # aaand symlink it
+    ln -sf /home/vagrant/code/webapp/www/ /home/vagrant/code/application/public/app
 fi
 
-# symlink it to public to we can access app-interface via webserver
-ln -sf /home/vagrant/code/webapp/www/ /home/vagrant/code/application/public/app
+
+
